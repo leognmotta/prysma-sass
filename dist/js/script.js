bd0cdeaa16da530f7451255f54f9686e3d69358d
Leonardo Motta <leo@domboscoatacado.com.br>
2018-08-30 08:44:36 -0300

    Add smooth scrool & Fiz problem with footer contact

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,6 +37,62 @@ window.onscroll = function() {
   }
 };
 
+// ################################
+// ########## SMOOTH SCROLL #######
+// ################################
+var link = document.querySelectorAll(".nav-item");
+
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+    for (var i = 0; i < link.length; i++) {
+      menuBtn.classList.remove("close");
+      menu.classList.remove("show");
+      menu.classList.add("hide");
+      // set Menu state
+      showMenu = false;
+    }
+  });
+}
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 90
+          },
+          1000,
+          function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            }
+          }
+        );
+      }
+    }
+  });
+
 // #################################
 
 var owl = $(".owl-carousel");
